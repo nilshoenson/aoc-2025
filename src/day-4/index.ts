5b19d99cf22a58e4d75d2ff8f3a427eb78b03921
Nils Hoenson <nils@ridecheck.app>
2025-12-04 19:20:53 +0100

    Add day 4, part 2

--- src/day-4/index.ts
+++ src/day-4/index.ts
@@ -45,6 +45,24 @@ function generateGridDots(lineAmount: number) {
   return dots
 }
 
+function getAdjacentRolls(entry: Dot, data: Dot[]) {
+  const xRange = calculateRange(entry.x)
+  const yRange = calculateRange(entry.y)
+
+  const arrWithinRange = data.filter(val => xRange.includes(val.x) && yRange.includes(val.y));
+  const arrWithoutValue = arrWithinRange.filter(val => val !== entry);
+
+  let rollsOfPaper = 0
+
+  for (const item of arrWithoutValue) {
+    if (item.value === ROLL_OF_PAPER) {
+      rollsOfPaper += 1
+    }
+  }
+
+  return rollsOfPaper
+}
+
 const ROLL_OF_PAPER = '@'
 const CUTOFF = 4
 
@@ -57,19 +75,7 @@ function partOne() {
   for (const entry of dots) {
     if (entry.value !== ROLL_OF_PAPER) continue
 
-    const xRange = calculateRange(entry.x)
-    const yRange = calculateRange(entry.y)
-
-    const arrWithinRange = dots.filter(val => xRange.includes(val.x) && yRange.includes(val.y));
-    const arrWithoutValue = arrWithinRange.filter(val => val !== entry);
-
-    let rollsOfPaper = 0
-
-    for (const item of arrWithoutValue) {
-      if (item.value === ROLL_OF_PAPER) {
-        rollsOfPaper += 1
-      }
-    }
+    const rollsOfPaper = getAdjacentRolls(entry, dots)
 
     if (rollsOfPaper < CUTOFF) {
       accessibleRows += 1
@@ -79,6 +85,49 @@ function partOne() {
   return accessibleRows
 }
 
+const REMOVED_PAPER = 'x'
+
+function partTwo() {
+  const numberOfLines = lines.length
+  let dots = generateGridDots(numberOfLines)
+
+  let totalRemovedRolls = 0
+
+  while (true) {
+    let accessibleRows = 0
+    let removedRolls = []
+
+    for (const entry of dots) {
+      if (entry.value !== ROLL_OF_PAPER) continue
+
+      const rollsOfPaper = getAdjacentRolls(entry, dots)
+
+      if (rollsOfPaper < CUTOFF) {
+        accessibleRows += 1
+        removedRolls.push(entry)
+      }
+    }
+
+    totalRemovedRolls += removedRolls.length
+
+    if (removedRolls.length === 0) {
+      break
+    }
+
+    for (const removed of removedRolls) {
+      const dot = dots.find(d => d.x === removed.x && d.y === removed.y)
+
+      if (dot) {
+        dot.value = REMOVED_PAPER
+      }
+    }
+  }
+
+  return totalRemovedRolls
+}
+
 const resultOne = partOne()
+const resultTwo = partTwo()
 
 console.log(`The total roles of paper that can be accessed are: ${resultOne}`)
+console.log(`The total roles of paper that can be accessed are: ${resultTwo}`)
